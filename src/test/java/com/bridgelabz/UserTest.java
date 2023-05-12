8d506609ea55ffd4bbca1e02930eaad7610bce7a
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-12 14:51:43 +0530

    Added:UserRegistratin20

---
+++ src/test/java/com/bridgelabz/UserTest.java
@@ -0,0 +1,77 @@
+package com.bridgelabz;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UserTest {
+    UserValidationTest user;
+
+    @Before
+    public void initialize() {
+        user = new UserValidationTest();
+    }
+
+    @Test
+    public void firstNameHappyTest() {
+        boolean firstName = user.checkFirstName("Rajnish");
+        Assert.assertEquals(true, firstName);
+    }
+
+    @Test
+    public void firstNameSadTest() {
+        boolean firstName = user.checkFirstName("Rajnish");
+        Assert.assertFalse(firstName);
+    }
+
+    @Test
+    public void checkLastNameHappyTest() {
+        boolean lastName = user.checkLastName("Kumar");
+        Assert.assertTrue(lastName);
+    }
+
+    @Test
+    public void checkLastNameSadTest() {
+        boolean lastName = user.checkLastName("raj");
+        Assert.assertFalse(lastName);
+    }
+
+    @Test
+    public void phoneNumberHappyTest() {
+        boolean phoneNumber = user.checkMobileNumber("917541061533");
+        Assert.assertTrue(phoneNumber);
+    }
+
+    @Test
+    public void phoneNumberBadTest() {
+        boolean phoneNumber = user.checkMobileNumber("12221 7541061533");
+        Assert.assertFalse(phoneNumber);
+    }
+
+    @Test
+    public void passwordHappyTest() {
+        boolean password = user.checkPassword("Rajnish123@");
+        Assert.assertTrue(password);
+    }
+
+    @Test
+    public void passwordSadTest() {
+        boolean password = user.checkPassword("Rajnish123@");
+        Assert.assertFalse(password);
+    }
+
+    @Test
+    public void emailHappyTest() {
+        boolean email = user.checkEmail("rajneeshsingh9905@gmai.com");
+        Assert.assertTrue(email);
+    }
+
+    @Test
+    public void emailSadTest() {
+        boolean email = user.checkEmail("rajneesh.@gmai.com");
+        Assert.assertFalse(email);
+    }
+}
